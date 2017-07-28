368a79d1fb0e8d2ad3b93aa14abd3bd4bf7ff877
scoiatael <czapl.luk+git@gmail.com>
2017-07-28 18:05:24 +0200

    Init
    based on anonymous code snippet from web

---
+++ statsd.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+func main() {
+	// listen to incoming udp packets
+	pc, err := net.ListenPacket("udp", ":8125")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer pc.Close()
+
+	blue := color.New(color.FgBlue).SprintFunc()
+	magenta := color.New(color.FgMagenta).SprintFunc()
+	green := color.New(color.FgGreen).SprintFunc()
+
+	buffer := make([]byte, 1024)
+	for {
+		pc.ReadFrom(buffer)
+		str := strings.Split(string(buffer), ":")
+		metric := str[0]
+		value := strings.Join(str[1:], ":")
+		fmt.Printf("[%s] StatsD Metric: %s %s\n", magenta(time.Now().String()), blue(metric), green(value))
+	}
+}
